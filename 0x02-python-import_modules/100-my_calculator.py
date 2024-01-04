285f5c7bbb4ccd5c6aa2c16c6a67b412a967915a
helmogey <h.elmogey@gmail.com>
2024-01-04 23:26:11 +0200

    3rd

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    args = sys.argv[1:]
+    if len(args) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    else:
+        op = args[1]
+        a = int(args[0])
+        b = int(args[2])
+        if op not in ["+", "-", "*", "/"]:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+        if op == "+":
+            print(add(a, b))
+        elif op == "-":
+            print(sub(a, b))
+        elif op == "*":
+            print(mul(a, b))
+        elif op == "/":
+            print(div(a, b))
