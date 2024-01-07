dac15bf6d755aa09aa1effd9d46d73756d53814b
helmogey <h.elmogey@gmail.com>
2024-01-07 16:10:25 +0200

    3rd

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,6 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(ele), end=" ")
             elif i == len(lst) - 1:
                 print("{:d}".format(ele), end="")
-
+            else:
+                print("")
         print("$")
