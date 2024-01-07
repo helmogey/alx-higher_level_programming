b4a58cda19b0b2b4898f9e70d04062c6f11094c2
helmogey <h.elmogey@gmail.com>
2024-01-07 16:06:09 +0200

    3rd

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,10 @@
 def print_matrix_integer(matrix=[[]]):
     for lst in matrix:
         for i, ele in enumerate(lst):
-            if i < len(lst):
+            if i < len(lst)- 1:
                 print("{:d}".format(ele), end=" ")
+            elif i == len(lst) - 1:
+                print("{:d}$".format(ele), end="")
             else:
                 print("")
         print()
