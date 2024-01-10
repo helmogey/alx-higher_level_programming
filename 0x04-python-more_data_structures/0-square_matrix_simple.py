c9f001b90dd826d6a5cfb105a1923e6f7645578b
helmogey <h.elmogey@gmail.com>
2024-01-10 11:50:29 +0200

    fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix:
-        out = []
-        for lst in matrix:
-            out.append(list(map(lambda x: x ** 2, lst)))
-        return out
+    out = []
+    for lst in matrix:
+        out.append(list(map(lambda x: x ** 2, lst)))
+    return out
+
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if search < len(my_list) and search >= 0:
-        out = []
-        for i in range(len(my_list)):
-            if my_list[i] == search:
-                out.append(replace)
-            else:
-                out.append(my_list[i])
-        return out
+    out = []
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            out.append(replace)
+        else:
+            out.append(my_list[i])
+    return out
