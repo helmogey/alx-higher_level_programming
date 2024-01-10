9b4a20d742af946acc6627afdfbb2d95c7886a26
helmogey <h.elmogey@gmail.com>
2024-01-10 11:45:31 +0200

    fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,8 +3,5 @@ def square_matrix_simple(matrix=[]):
     if matrix:
         out = []
         for lst in matrix:
-            tmp = []
-            for l in lst:
-                tmp.append(l**2)
-            out.append(tmp)
+            out.append(list(map(lambda x: x ** 2, lst)))
         return out
