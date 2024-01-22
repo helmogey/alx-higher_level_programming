cdc3223ca0cdeafe5e110f28d6dc71aef22e07b9
helmogey <h.elmogey@gmail.com>
2024-01-22 12:59:27 +0200

    print

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
     try:
         1 + value
         print(value)
         return True
     except Exception as e:
-        print(e)
+        print(f"Exception: {e}", file=sys.stderr)
         return False
