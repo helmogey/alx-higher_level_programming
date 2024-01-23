d01a8b89edd743d3d2e770573abeecc33fedf345
helmogey <h.elmogey@gmail.com>
2024-01-23 20:27:14 +0200

    print

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     c = None
     try:
         c = a/b
-    except:
+    except Exception as e:
         pass
     finally:
         print("Inside result: {}".format(c))
