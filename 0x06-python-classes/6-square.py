8c2e1773befe7506d3fce1733cf1e51a0b914caf
helmogey <h.elmogey@gmail.com>
2024-01-23 22:23:28 +0200

    print

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,9 +32,8 @@ class Square:
         if self.__size <= 0:
             print()
         else:
-            if self.__position[1] ==0:
-                for i in range(0, self.__position[1]):
-                    print("")
+            for i in range(0, self.__position[1]):
+                print("")
             for i in range(0, self.__size):
                 for j in range(0, self.__position[0]):
                     print(" ", end="")
