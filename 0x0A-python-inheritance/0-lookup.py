e0d06d5a158b2c131ab7c7aaa68142cb32046716
helmogey <h.elmogey@gmail.com>
2024-02-05 11:09:15 +0200

    0A

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -9,17 +9,4 @@ def lookup(obj):
       Returns:
           A list of strings, where each string represents an attribute or method name.
       """
-    attributes = []
-    methods = []
-    # Get all attributes and methods of the object
-    for name in dir(obj):
-        # Filter out private and special methods
-        if name.startswith("__"):
-            # Check if it's an attribute or method
-            if hasattr(obj, name):
-                attributes.append(name)
-            else:
-                methods.append(name)
-
-    # Combine attributes and methods into a single list
-    return attributes + methods
+    return (dir(obj))
