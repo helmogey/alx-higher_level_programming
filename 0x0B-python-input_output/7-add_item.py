e7f0897d503a0b80b3e47712f2b58c0766a9d7d5
helmogey <h.elmogey@gmail.com>
2024-02-06 13:16:43 +0200

    0B

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,15 +2,23 @@
 """ function that writes an Object to a text file, using a JSON representation:"""
 import sys
 
+
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+    filename = "add_item.json"
 
     try:
-        items = load_from_json_file("add_item.json")
+        # Load existing items from file, if any
+        existing_items = load_from_json_file(filename)
     except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+        existing_items = []  # Create an empty list if the file doesn't exist
+
+    # Combine existing items with new items
+    all_items = existing_items + sys.argv[1:]
+
+    # Save the updated list to the file
+    save_to_json_file(all_items, filename)
 
