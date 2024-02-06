0d32fc151545c9cb970b67d937fe3abdfa0e9e80
helmogey <h.elmogey@gmail.com>
2024-02-06 13:12:55 +0200

    0B

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 """ function that writes an Object to a text file, using a JSON representation:"""
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-filename = "add_item.json"
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    # Load existing items from file, if any
-    existing_items = load_from_json_file(filename)
-except FileNotFoundError:
-    existing_items = []  # Create an empty list if the file doesn't exist
 
-# Combine existing items with new items
-all_items = existing_items + sys.argv[1:]
+    filename = "add_item.json"
 
-# Save the updated list to the file
-save_to_json_file(all_items, filename)
+    try:
+        # Load existing items from file, if any
+        existing_items = load_from_json_file(filename)
+    except FileNotFoundError:
+        existing_items = []  # Create an empty list if the file doesn't exist
+
+    # Combine existing items with new items
+    all_items = existing_items + sys.argv[1:]
+
+    # Save the updated list to the file
+    save_to_json_file(all_items, filename)
+
