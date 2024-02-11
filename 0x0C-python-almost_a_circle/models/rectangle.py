b9dc2fbc8b86350e4808d668bd17fbb0e65dc25a
helmogey <h.elmogey@gmail.com>
2024-02-11 10:15:32 +0200

    0xc

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+"""Write the first class Rectangle that inherits from Base"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle that inherits from Base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize the rectangle"""
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Set/get width of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Set/get height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Set/get x coordinate of the Rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Set/get y coordinate of the Rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Calculate the area of the Rectangle"""
+        return self.width * self.height
+
+    def display(self):
+        """Display the rectangle"""
+
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
+
+        for i in range(self.height):
+            [print('#', end="") for j in range(self.width)]
+            if i != self.height - 1:
+                print("")
+        print("")
+
+
+    def __str__(self):
+        """overriding the __str__ method"""
+
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,self.x, self.y,self.width, self.height)
