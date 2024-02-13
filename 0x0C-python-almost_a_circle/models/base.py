1999d3feb8922508c913e4a69731f6169c3b15ed
helmogey <h.elmogey@gmail.com>
2024-02-13 10:13:54 +0200

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,7 +3,14 @@
 
 import json
 class Base:
-    """"""
+    """Base model.
+
+    This Represents the "base" for all other classes in project 0x0C*.
+
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Bases.
+    """
+    
     __nb_objects = 0
 
     def __init__(self, id=None):
