29c1f7d77960cca1c7c80d2397d83d7fe3495a4c
helmogey <h.elmogey@gmail.com>
2024-03-12 15:12:29 +0200

    sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- script that displays the average temperature (Fahrenheit).
-SELECT city, AVG(value) AS average_temperature_fahrenheit
+SELECT city, AVG(`value`) AS average_temperature_fahrenheit
 FROM temperatures
 GROUP BY city
 ORDER BY average_temperature_fahrenheit DESC;
