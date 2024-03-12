e86bf3dd0dd4de09b083d11efcb877333a6f7f23
helmogey <h.elmogey@gmail.com>
2024-03-12 13:49:42 +0200

    sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- Create the second_table in the hbtn_0c_0 database if it does not exist
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
