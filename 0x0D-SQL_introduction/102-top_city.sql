3357de7c1f6c81545cf228a6aaf9ff05bd1d9a6d
helmogey <h.elmogey@gmail.com>
2024-03-12 15:10:05 +0200

    sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
 SELECT city, MAX(temperature) AS max_temperature
 FROM temperatures
-WHERE month IN ('July', 'August')
+WHERE month IN (7, 8)
 GROUP BY city
 ORDER BY max_temperature DESC
 LIMIT 3;
