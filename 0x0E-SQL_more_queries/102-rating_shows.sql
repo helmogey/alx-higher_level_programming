027adc4b50a9e22aced3f94683c9c72c8e683955
helmogey <h.elmogey@gmail.com>
2024-03-13 12:50:22 +0200

    0XE

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,7 +2,7 @@
 -- Each record should display: tv_shows.title - rating sum
 -- Results must be sorted in descending order by the rating
 -- You can use only one SELECT statement
-SELECT tv_shows.title, SUM(hbtn_0d_tvshows_rate.rating) AS rating_sum
+SELECT tv_shows.title, SUM(tv_show_ratings.rating) rating_sum
 FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
