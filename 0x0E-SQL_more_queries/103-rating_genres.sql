489eaa445972ab5ad6c3e0eac0b0b1531c6eae2c
helmogey <h.elmogey@gmail.com>
2024-03-13 12:58:47 +0200

    0XE

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -5,6 +5,7 @@
 SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS 'rating'
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tvshows_rate ON tv_show_genres.show_id = tvshows_rate.show_id
+JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY tv_genres.name
 ORDER BY rating DESC;
+
