b657ed12fbda79f319cb2e4a4a11af4102258178
helmogey <h.elmogey@gmail.com>
2024-03-13 11:17:20 +0200

    0XE

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- script that creates the database hbtn_0d_usa and the table states
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 use hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT PRIMARY KEY,
