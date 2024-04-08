c4dc9261957d49feb2ce5a491eb61386575afcb2
helmogey <h.elmogey@gmail.com>
2024-04-08 14:11:42 +0200

    java script 1st

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,11 +4,8 @@ const args = process.argv.slice(2);
 function add (a, b) {
   const num1 = parseInt(a);
   const num2 = parseInt(b);
-  if (isNaN(num1) || isNaN(num2)) {
-    return 'Not a number';
-  } else {
-    return num1 + num2;
-  }
+
+  return num1 + num2;
 }
 
 const result = add(args[0], args[1]);
