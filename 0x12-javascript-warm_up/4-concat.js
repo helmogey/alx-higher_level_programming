5633dbecb789bbd7bb9b8780273e8c517bbcba39
helmogey <h.elmogey@gmail.com>
2024-04-08 13:47:47 +0200

    java script 1st

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,6 +3,4 @@ const args = process.argv.slice(2);
 
 if (args.length >= 2) {
   console.log(`${args[0]} is ${args[1]}`);
-} else {
-  console.log('Not enough arguments! Please provide two arguments.');
 }
