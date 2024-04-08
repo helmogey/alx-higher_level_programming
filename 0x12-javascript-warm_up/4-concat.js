f11549fd88854c8aad3592f0678ee862ff7ce15c
helmogey <h.elmogey@gmail.com>
2024-04-08 13:46:35 +0200

    java script 1st

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const args = process.argv.slice(2); // Get arguments excluding script name and path
+const args = process.argv.slice(2);
 
 if (args.length >= 2) {
   console.log(`${args[0]} is ${args[1]}`);
