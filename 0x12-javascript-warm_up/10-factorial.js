84af6e204142003d4d1b13efce02c9076390c415
helmogey <h.elmogey@gmail.com>
2024-04-08 14:15:08 +0200

    java script 1st

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+
+function factorial(num) {
+  const number = parseInt(num);
+  if (isNaN(number)) {
+    return 1;
+  } else if (number === 0 || number === 1) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
+}
+
+const result = factorial(args[0]);
+console.log(result);
