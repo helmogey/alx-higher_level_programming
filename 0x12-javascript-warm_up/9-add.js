b731b504751e98a948911d678dd530ee2241c0e1
helmogey <h.elmogey@gmail.com>
2024-04-08 14:09:58 +0200

    java script 1st

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,19 +1,16 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 
-if (args.length < 2) {
-  console.log('Missing arguments! Please provide two numbers.');
-} else {
-  function add (a, b) {
-    const num1 = parseInt(a);
-    const num2 = parseInt(b);
-    if (isNaN(num1) || isNaN(num2)) {
-      return 'Not a number';
-    } else {
-      return num1 + num2;
-    }
+function add (a, b) {
+  const num1 = parseInt(a);
+  const num2 = parseInt(b);
+  if (isNaN(num1) || isNaN(num2)) {
+    return 'Not a number';
+  } else {
+    return num1 + num2;
   }
-
-  const result = add(args[0], args[1]);
-  console.log(result);
 }
+
+const result = add(args[0], args[1]);
+console.log(result);
+
