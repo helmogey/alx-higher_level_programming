83ca88adab1f159a6172d207f017662f71bb93e5
helmogey <h.elmogey@gmail.com>
2024-04-08 13:52:10 +0200

    java script 1st

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const firstArg = args[0];
+
+if (isNaN(firstArg)) {
+  console.log("Not a number");
+} else {
+  console.log(`My number: ${parseInt(firstArg)}`);
+}
