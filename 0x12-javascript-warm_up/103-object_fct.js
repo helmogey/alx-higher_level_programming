0b3c1c59963e925ae921c273b18cea7af462fea6
helmogey <h.elmogey@gmail.com>
2024-04-09 11:44:58 +0200

    java script 1st

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-function incr(obj) {
-  obj.value++;
-}
+myObject.incr = function () {
+  this.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
