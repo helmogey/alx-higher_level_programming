b0d31288f73fbcb3b36e59424537a97aa9d9c2e9
helmogey <h.elmogey@gmail.com>
2024-04-09 15:03:51 +0200

    java script 1st

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,14 +5,7 @@ const sourceFile1 = process.argv[2];
 const sourceFile2 = process.argv[3];
 const destinationFile = process.argv[4];
 
-let combinedContent = '';
-
-fs.readFile(sourceFile1, 'utf8', (err1, data1) => {
-  combinedContent += data1;
-
-  fs.readFile(sourceFile2, 'utf8', (err2, data2) => {
-    combinedContent += data2;
-
-    fs.writeFileSync(destinationFile, combinedContent);
-  });
-});
+let data1 = fs.readFileSync(sourceFile1.toString());
+let data2 = fs.readFileSync(sourceFile2.toString());
+let combinedContent = data1 + data2;
+fs.writeFileSync(destinationFile, combinedContent);
