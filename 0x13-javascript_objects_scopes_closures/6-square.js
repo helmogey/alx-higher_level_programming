4e29636713defaf71e4a470ef182397284735847
helmogey <h.elmogey@gmail.com>
2024-04-09 12:24:07 +0200

    java script 1st

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Squareold = require('5-square');
+class Square extends Squareold {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c = 'X') {
+    let output = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        output += c;
+      }
+      output += '\n';
+    }
+    process.stdout.write(output);
+  }
+}
+module.exports = Square;
