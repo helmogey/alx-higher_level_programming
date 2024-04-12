7b8c598b75a1f91090afdec0bf16936cbad35304
helmogey <h.elmogey@gmail.com>
2024-04-12 14:49:38 +0200

    sql

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -21,8 +21,8 @@ def list_states(username, password, database, search_name):
     cursor = connection.cursor()
 
     # Execute query to select all states ordered by id
-    query = "SELECT * FROM states WHERE name = %s"
-    cursor.execute(query, (search_name,))
+    query = "SELECT * FROM states WHERE name = " + str(search_name)
+    cursor.execute(query)
 
 
     # Fetch results and print them
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -21,7 +21,7 @@ def list_states(username, password, database, search_name):
     cursor = connection.cursor()
 
     # Execute query to select all states ordered by id
-    query = "SELECT * FROM states WHERE name = %s"
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
     cursor.execute(query, (search_name,))
 
 
