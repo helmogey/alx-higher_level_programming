78606e00e7d9cd875f080b9258aa8d4f76fba107
helmogey <h.elmogey@gmail.com>
2024-04-13 21:16:06 +0200

    sec

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" 1 filter states """
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+def list_states(username, password, database):
+    """
+    Connects to a MySQL database and lists all states from the 'states' table,
+    sorting them by id in ascending order.
+
+    Args:
+        username: Username for MySQL authentication.
+        password: Password for MySQL authentication.
+        database: Name of the database to connect to.
+    """
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database))
+    # Call list_all_states function
+    Base.metadata.create_all(engine)
+
+    # Create a session using the engine
+    SessionLocal = sessionmaker(bind=engine)
+    session = SessionLocal()
+
+    state = session.query(State).filter(State.name == (sys.argv[4],))
+    try:
+        print(state[0].id)
+    except IndexError:
+        print("Not found")
+
+
+if __name__ == "__main__":
+    # Create argument parser
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    # Parse arguments
+
+    list_states(username, password, database)
+
+
