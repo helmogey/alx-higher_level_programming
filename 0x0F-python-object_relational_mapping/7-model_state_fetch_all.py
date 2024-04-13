cdda615a488a10182fab57f8b6467ca752d80cda
helmogey <h.elmogey@gmail.com>
2024-04-13 20:54:10 +0200

    sec

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ 1 filter states """
 
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
