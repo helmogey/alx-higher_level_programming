e6e276e496bd0a72be23a0890daf9d58e77af7c3
helmogey <h.elmogey@gmail.com>
2024-04-13 20:56:41 +0200

    sec

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,26 +6,37 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
+
+def list_states(username, password, database):
+    """
+    Connects to a MySQL database and lists all states from the 'states' table,
+    sorting them by id in ascending order.
+
+    Args:
+        username: Username for MySQL authentication.
+        password: Password for MySQL authentication.
+        database: Name of the database to connect to.
+    """
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database))
+    # Call list_all_states function
+    Base.metadata.create_all(engine)
+
+    # Create a session using the engine
+    SessionLocal = sessionmaker(bind=engine)
+    session = SessionLocal()
+
+    states = session.query(State).order_by(State.id)
+
+    # Print state information
+    for state in states:
+        print(state.id, state.name, sep=": ")
+
 if __name__ == "__main__":
-  # Create argument parser
+    # Create argument parser
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    # Parse arguments
 
-
-  username = sys.argv[1]
-  password = sys.argv[2]
-  database = sys.argv[3]
-  # Parse arguments
-
-  engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                         .format(username, password, database))
-  # Call list_all_states function
-  Base.metadata.create_all(engine)
-
-  # Create a session using the engine
-  SessionLocal = sessionmaker(bind=engine)
-  session = SessionLocal()
-
-  states = session.query(State).order_by(State.id)
-
-  # Print state information
-  for state in states:
-      print(state.id, state.name, sep=": ")
+    list_states(username, password, database)
