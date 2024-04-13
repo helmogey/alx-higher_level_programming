f4ef9b2b6299170e57971d60343be703df4cba7a
helmogey <h.elmogey@gmail.com>
2024-04-13 20:44:05 +0200

    sec

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,50 +1,17 @@
 #!/usr/bin/python3
-""" 1 filter states """
-
-from sqlalchemy import create_engine, Session
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+"""Start link class to table in database
+"""
 import sys
-def list_all_states(username, password, database):
-  """
-  Connects to a MySQL database, creates a session, and lists all State objects
-  from the 'states' table, sorted by id in ascending order.
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
-  Args:
-      username: Username for MySQL authentication.
-      password: Password for MySQL authentication.
-      database: Name of the database to connect to.
-  """
-
-  # Create database engine connection URL
-  engine = create_engine(f'mysql://{username}:{password}@localhost:3306/{database}')
-
-  # Create a session using the engine
-  SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-  session = SessionLocal()
-
-  try:
-    # Query all State objects ordered by id
-    states = session.query(State).order_by(State.id)
-
-    # Print state information
-    for state in states:
-      print(state.id, state.name, sep=": ")
-
-  except Exception as err:
-    pass
-  finally:
-    # Close the session
-    session.close()
 
 if __name__ == "__main__":
-  # Create argument parser
-
-
-  username = sys.argv[1]
-  password = sys.argv[2]
-  database = sys.argv[3]
-  # Parse arguments
-
-  # Call list_all_states function
-  list_all_states(username, password, database)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
