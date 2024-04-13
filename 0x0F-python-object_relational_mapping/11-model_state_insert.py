c5a90175735f6056154d1c2babd65a71a3dc10e1
helmogey <h.elmogey@gmail.com>
2024-04-13 22:01:55 +0200

    sec

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,48 +1,20 @@
 #!/usr/bin/python3
-""" 1 filter states """
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+""" prints the State object with the name passed as argument from the database
+"""
 import sys
-
-
-def list_states(username, password, database, state_name='Louisiana'):
-    """
-    Connects to a MySQL database and lists all states from the 'states' table,
-    sorting them by id in ascending order.
-
-    Args:
-        username: Username for MySQL authentication.
-        password: Password for MySQL authentication.
-        database: Name of the database to connect to.
-    """
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database))
-    # Call list_all_states function
-    Base.metadata.create_all(engine)
-
-    # Create a session using the engine
-    SessionLocal = sessionmaker(bind=engine)
-    session = SessionLocal()
-
-    new_state = State(name=state_name)
-
-    # Add the new state to the session
-    session.add(new_state)
-
-    new_instance = session.query(State).filter_by(name='Louisiana').first()
-    print(new_instance.id)
-    # Commit changes to the database
-    session.commit()
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    # Create argument parser
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]    # Parse arguments
-
-    list_states(username, password, database)
-
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    new_instance = session.query(State).filter_by(name='Louisiana').first()
+    print(new_instance.id)
+    session.commit()
