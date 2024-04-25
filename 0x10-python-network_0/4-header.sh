f2319f5f87c91a60c321eee95bf09bfe45d6066e
helmogey <h.elmogey@gmail.com>
2024-04-25 20:30:49 +0200

    first

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a POST request to the passed URL
+# Bash script that takes in a URL as an argument, sends a GET request to the URL
 curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
