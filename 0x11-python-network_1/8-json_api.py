d8f7e5e20440b46f033ce5fe3e1926d413f55852
helmogey <h.elmogey@gmail.com>
2024-04-26 23:09:49 +0300

    py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,32 +9,30 @@ import json
 def main():
     """Sends a POST request with a letter parameter and displays user information."""
 
-    if len(sys.argv) > 1:
-        q = sys.argv[1]
-    else:
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
+
+    if len(sys.argv) == 1:
         q = ""
+    else:
+        q = sys.argv[1]
     data = {"q": q}
     url = "http://0.0.0.0:5000/search_user"
-    response = requests.post(url, data=data)
-
-    if response.status_code == 200:
-        try:
-            data = json.loads(response.text)
-
-            # Check if the parsed data is a dictionary and not empty
-            if isinstance(data, dict) and data:
-                # Extract and display user information
-                user_id = data.get("id")
-                user_name = data.get("name")
-                print(f"[{user_id}] {user_name}")
-            else:
-                # JSON is empty
-                print("No result")
-        except json.JSONDecodeError:
-            # Invalid JSON format
-            print("Not a valid JSON")
-    else:
-        print(f"Error: Request failed with status code {response.status_code}")
+    r = requests.post(url, data=data)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
 
 
 if __name__ == "__main__":
