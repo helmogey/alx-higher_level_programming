f2c80ffd128d527a94719f9832a93145b942e9a1
helmogey <h.elmogey@gmail.com>
2024-04-26 22:57:09 +0300

    py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,20 @@
+
+"""script that takes in a URL and an email address, sends a POST
+request to the passed URL with the email as a parameter,
+and finally displays the body of the response."""
+
+import requests
+import sys
+
+def main(url, email):
+    """Sends a POST request to a URL with an email
+     parameter and displays the response body."""
+
+    data = {"email": email}
+    response = requests.post(url, data=data)
+    if response.status_code == 200:
+        # Display the response body
+        print(dict(response.headers).get("X-Request-Id"))
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])
