f5117de54e173b092f64fafcba270b8b1aa4fab7
helmogey <h.elmogey@gmail.com>
2024-04-26 23:10:34 +0300

    py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,40 +1,23 @@
 #!/usr/bin/python3
-"""script that takes in a letter and sends a POST request to
- http://0.0.0.0:5000/search_user with the letter as a parameter."""
-import requests
+"""A script tha:
+- takes in a letter
+- sends POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
 import sys
-import json
-
-
-def main():
-    """Sends a POST request with a letter parameter and displays user information."""
-
-    try:
-        response = r.json()
-        if response == {}:
-            print("No result")
-        else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
-    except ValueError:
-        print("Not a valid JSON")
-
-    if len(sys.argv) == 1:
-        q = ""
-    else:
-        q = sys.argv[1]
-    data = {"q": q}
-    url = "http://0.0.0.0:5000/search_user"
-    r = requests.post(url, data=data)
-    try:
-        response = r.json()
-        if response == {}:
-            print("No result")
-        else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
-    except ValueError:
-        print("Not a valid JSON")
+import requests
 
 
 if __name__ == "__main__":
-    main()
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
